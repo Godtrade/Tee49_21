8ea36a1289afc2fd780a2da5c4c4d3956de6ff39
Teerapong <teerapongbangsantia@gmail.com>
2026-03-06 15:09:01 +0700

    completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
 
+int main(int argc, char* argv[]) {
+   
+    if (argc == 1) {
+        std::cout << "Please input numbers to find average." << std::endl;
+        return 0;
+    }
+
+    double sum = 0.0;
+    int count = argc - 1;
+
+    for (int i = 1; i < argc; ++i) {
+        sum += std::stod(argv[i]); 
+    }
+
+    double average = sum / count;
+
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "Average of " << count << " numbers = " << average << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    return 0;
+}
--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,63 @@
+#undef UNICODE
+#undef _UNICODE
 #include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ID_INPUT1 101
+#define ID_INPUT2 102
+#define ID_BTN_ADD 201
+#define ID_BTN_SUB 202
+#define ID_BTN_MUL 203
+#define ID_BTN_DIV 204
+
+HWND hInput1, hInput2;
 
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
-		
+		case WM_CREATE: {
+            CreateWindow("STATIC", "Number 1:", WS_VISIBLE | WS_CHILD, 10, 20, 70, 20, hwnd, NULL, NULL, NULL);
+            hInput1 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL, 80, 20, 140, 20, hwnd, (HMENU)ID_INPUT1, NULL, NULL);
+
+            CreateWindow("STATIC", "Number 2:", WS_VISIBLE | WS_CHILD, 10, 55, 70, 20, hwnd, NULL, NULL, NULL);
+            hInput2 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL, 80, 55, 140, 20, hwnd, (HMENU)ID_INPUT2, NULL, NULL);
+
+            CreateWindow("BUTTON", "+", WS_VISIBLE | WS_CHILD, 15, 100, 45, 45, hwnd, (HMENU)ID_BTN_ADD, NULL, NULL);
+            CreateWindow("BUTTON", "-", WS_VISIBLE | WS_CHILD, 65, 100, 45, 45, hwnd, (HMENU)ID_BTN_SUB, NULL, NULL);
+            CreateWindow("BUTTON", "*", WS_VISIBLE | WS_CHILD, 115, 100, 45, 45, hwnd, (HMENU)ID_BTN_MUL, NULL, NULL);
+            CreateWindow("BUTTON", "/", WS_VISIBLE | WS_CHILD, 165, 100, 45, 45, hwnd, (HMENU)ID_BTN_DIV, NULL, NULL);
+            break;
+        }
+		case WM_COMMAND: {
+            int wmId = LOWORD(wParam);
+            if (wmId >= ID_BTN_ADD && wmId <= ID_BTN_DIV) {
+                char buf1[100], buf2[100];
+                
+                GetWindowText(hInput1, buf1, 100);
+                GetWindowText(hInput2, buf2, 100);
+
+                double num1 = atof(buf1);
+                double num2 = atof(buf2);
+                double result = 0;
+                char t[100]; 
+
+                if (wmId == ID_BTN_ADD) result = num1 + num2;
+                else if (wmId == ID_BTN_SUB) result = num1 - num2;
+                else if (wmId == ID_BTN_MUL) result = num1 * num2;
+                else if (wmId == ID_BTN_DIV) {
+                    if (num2 != 0) result = num1 / num2;
+                    else {
+                        MessageBox(hwnd, "Cannot divide by zero!", "Error", MB_OK | MB_ICONERROR);
+                        break;
+                    }
+                }
+
+                sprintf(t, "%f", result); // เปลี่ยนตัวเลขเป็น c-string
+                MessageBox(hwnd, t, "Result", MB_OK | MB_ICONINFORMATION); // แสดง Message Box
+            }
+            break;
+        }
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
 			PostQuitMessage(0);
@@ -31,7 +85,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = CreateSolidBrush(RGB(255, 255, 0));
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,11 +95,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE | WS_CAPTION | WS_SYSMENU,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
